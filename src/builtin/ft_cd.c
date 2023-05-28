bc468981c307ccead3bede310ac921c475544094
cerapcapiq <103482299+cerapcapiq@users.noreply.github.com>
2023-05-28 13:51:49 +0800

    Add files via upload

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/12 12:59:53 by abasarud          #+#    #+#             */
-/*   Updated: 2023/05/26 16:18:31 by abasarud         ###   ########.fr       */
+/*   Updated: 2023/05/26 18:07:38 by abasarud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,21 @@
 // 	s = ft_var_content("OLDPWD", head_ref);
 // 	return (s);
 // }
-
-int	cd(int argc, char **argv)
+#include <string.h>
+int	cd(int argc, char **argv, Node* envListHead)
 {
-	// char	*now;
-	// char	*curr;
-	// char	arg[1024];
+		(void) argc;
+	 char *old_pwd[] = {"OLDPWD", "OLDPWD", "OLDPWD"};
+	 char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+{
+		deleteNode(&envListHead, &old_pwd[0]);
+		insertNode(&envListHead, *old_pwd, cwd);
+}
+else 
+printf("error\n");
+// (void) mini;
 
-	(void) argc;
 	// if (ft_strcmp(argv[1], "-"))
 	// {
 	// 	now = getcwd(arg, 1024);
@@ -60,7 +67,23 @@ int	cd(int argc, char **argv)
 	// }
 	// else
 	// {
+
+		// char *now = getcwd(*argv, 1024);
+		// char *old_pwd = "OLDPWD";
+		// printf("now is %s", now);
+		// printf("old path %s", old_pwd);
+		// deleteNode(&mini->envListHead, &old_pwd);
+		// insertNode(&mini->envListHead, old_pwd, now);
 		chdir(argv[1]);
+		char curr[1024];
+		char *pwd[] = {"PWD", "PWD"};
+    if (getcwd(curr, sizeof(curr)) != NULL)
+{
+
+		deleteNode(&envListHead, &pwd[0]);
+		insertNode(&envListHead, *pwd, curr);
+}
+
 	// }
 	return (0);
 }
