774b49c82e454b2e53ffb2550d5709a6498049b6
cerapcapiq <103482299+cerapcapiq@users.noreply.github.com>
2023-05-30 16:39:17 +0800

    Add files via upload

--- src/main/main.c
+++ src/main/main.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 13:18:36 by abasarud          #+#    #+#             */
+/*   Updated: 2023/05/30 16:11:09 by abasarud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 t_mini	*init_mini(void)
 {
-	t_mini  *mini;
+	t_mini	*mini;
 
 	mini = (t_mini *)malloc(sizeof(t_mini));
 	mini->tokens = 0;
@@ -16,8 +27,8 @@ t_mini	*init_mini(void)
 	mini->stdout = dup(STDOUT_FILENO);
 	mini->execute_code = 0;
 	mini->env = NULL;
-	mini->envListHead = NULL;
-	mini->varListHead = NULL;
+	mini->env_list = NULL;
+	mini->var_list = NULL;
 	return (mini);
 }
 
@@ -47,36 +58,36 @@ int	ft_whitespace(char *input)
 	return (0);
 }
 
-static void read_input(t_mini *mini, char *input)
+static void	read_input(t_mini *mini, char *input)
 {
 	input = display_readline(input);
 	if (!ft_whitespace(input))
 	{
-        parse(mini, input);
-	    add_history(input);
-	    dup2(mini->stdin, STDIN_FILENO);
-	    dup2(mini->stdout, STDOUT_FILENO);
+		parse(mini, input);
+		add_history(input);
+		dup2(mini->stdin, STDIN_FILENO);
+		dup2(mini->stdout, STDOUT_FILENO);
 	}
 	free(input);
 }
 
-int main(int ac, char *av[], char *envp[])
+int	main(int ac, char *av[], char *envp[])
 {
-    t_mini  *mini;
-    char    *input;
-    g_exit_num = 0;
+	t_mini	*mini;
+	char	*input;
 
-    define_signal();
-    init_main(ac, av);
-    mini = init_mini();
-    mini->varListHead = var_initializeMyNode();
-	mini->envListHead = getTerminalEnvList(envp);
+	g_exit_num = 0;
+	define_signal();
+	init_main(ac, av);
+	mini = init_mini();
+	mini->var_list = var_initialize_var();
+	mini->env_list = get_terminal_env_list(envp);
 	while (1)
-    {
-        input = NULL;
-        read_input(mini, input);
-    }
-    free_Head(mini->envListHead);
-	free_Var(mini->varListHead);
+	{
+		input = NULL;
+		read_input(mini, input);
+	}
+	free_head(mini->env_list);
+	free_var(mini->var_list);
 	return (mini->execute_code);
 }
