43c1cb3816581b2077038ae9f400d6e144e4e113
cerapcapiq <103482299+cerapcapiq@users.noreply.github.com>
2023-05-30 18:55:48 +0800

    Add files via upload

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 13:04:49 by abasarud          #+#    #+#             */
-/*   Updated: 2023/05/30 15:43:56 by abasarud         ###   ########.fr       */
+/*   Updated: 2023/05/30 18:27:25 by abasarud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	execute(t_mini *mini)
 	char	**argv;
 	t_token	*tok;
 	t_token	*command;
-	int		i;
-// char	exit_status[16];
 
 	tok = mini->tokens;
 	while (tok)
@@ -28,7 +26,17 @@ int	execute(t_mini *mini)
 		tok = tok->next;
 		while (tok && tok->type == ARG)
 			tok = tok->next;
-		i = 0;
+		call_pipe_redirect(mini, command, tok);
+		if (command->type == BUILTIN)
+			mini->execute_code = execute_builtin(argv, command->str, mini);
+		else if (command->type == CMD)
+			mini->execute_code = call_cmd(argv, mini);
+	}
+	wait(NULL);
+	return (0);
+}
+
+		// i = 0;
 		// while (argv[i] != NULL)
 		// {
 		// 		printf("inside howute\n\n");
@@ -39,12 +47,3 @@ int	execute(t_mini *mini)
 		// 	}
 		// 	i++;
 		// }
-		call_pipe_redirect(mini, command, tok);
-		if (command->type == BUILTIN || command->type == VAR)
-			mini->execute_code = execute_builtin(argv, command->str, mini);
-		else if (command->type == CMD)
-			mini->execute_code = call_cmd(argv, mini);
-	}
-	wait(NULL);
-	return (0);
-}
