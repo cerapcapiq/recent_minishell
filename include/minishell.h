2675b918047e0c79ea4447114fc939ed7fe26208
cerapcapiq <103482299+cerapcapiq@users.noreply.github.com>
2023-05-30 15:47:23 +0800

    Add files via upload

--- include/minishell.h
+++ include/minishell.h
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 14:39:31 by abasarud          #+#    #+#             */
+/*   Updated: 2023/05/30 14:45:28 by abasarud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -15,7 +26,7 @@
 # include <termios.h>
 # include <stdbool.h>
 # include "../libft/libft.h"
-#include <sys/stat.h>
+# include <sys/stat.h>
 
 # define BUILTIN 1
 # define CMD 2
@@ -31,19 +42,19 @@
 # define PROMPT		0
 # define EOT		100
 
-int g_exit_num;
+typedef struct Node
+{
+	char		*name;
+	char		*value;
+	struct Node	*next;
+}	t_node;
 
-typedef struct Node {
-    char* name;
-    char* value;
-    struct Node* next;
-} Node;
-
-typedef struct MyNode {
-    char* name;
-    char* value;
-    struct MyNode* next;
-} MyNode;
+typedef struct My_node
+{
+	char			*name;
+	char			*value;
+	struct My_node	*next;
+}	t_var;
 
 typedef struct s_token
 {
@@ -66,80 +77,80 @@ typedef struct s_mini
 	int			stdout;
 	int			execute_code;
 	t_list		*env;
-	Node* envListHead;
-	MyNode*   varListHead;
+	t_node		*env_list;
+	t_var		*var_list;
 }	t_mini;
 
-
+int		g_exit_num;
 //parse
 
 void	parse(t_mini *mini, char *input);
 char	**ft_new_split(char *s, char c);
-char* ft_strtok(char* str, const char* delimiters);
-int	ft_strcmp(char *s1, char *s2);
-char* add_spaces_around_pipe(const char* s);
+char	*ft_strtok(char *str, const char *delimiters);
+int		ft_strcmp(char *s1, char *s2);
+char	*add_spaces_around_pipe(const char *s);
 
 //token
 t_token	*new_token(t_mini *mini, char *data);
 void	token_addend(char *data, t_mini *mini);
 
 //quote
-int	closed_quote(char *line);
-int	closed_dub_quote(char *line);
-int	ft_dollar(char *new);
-char *ft_delete_quote(char* data);
-char *ft_delete_quote_sin(char* data);
-int	detect_q(char *data);
-int	ft_double_detect_quote(char *data);
+int		closed_quote(char *line);
+int		closed_dub_quote(char *line);
+int		ft_dollar(char *new);
+char	*ft_delete_quote(char *data);
+char	*ft_delete_quote_sin(char *data);
+int		detect_q(char *data);
+int		ft_double_detect_quote(char *data);
 
 //execute
-int	execute(t_mini *mini);
+int		execute(t_mini *mini);
 char	*ft_strremove(char *str, const char *sub);
-int	count_argc(char **args);
+int		count_argc(char **args);
 char	**convert_argv(t_token	*tokens);
-int	execute_builtin(char **argv, char *command, t_mini *ms);
+int		execute_builtin(char **argv, char *command, t_mini *ms);
 
 //builtin
-int findSubstring(const char* string, const char* substring);
-void	ft_export(char **str, Node* envListHead, MyNode *varListHead);
-int	echo(int argc, char **argv, t_token curr, MyNode *varListHead, Node* envListHead);
-int     cd(int argc, char **argv, Node* envListHead);
-int	pwd(void);
-int	mini_exit(char **argv);
+int		find_substring(const char *string, const char *substring);
+void	ft_export(char **str, t_node *env_list, t_var *var_list);
+int		echo(int argc, char **argv, t_token curr, t_var *var_list, t_node *env_list);
+int		cd(int argc, char **argv, t_node *env_list);
+int		pwd(void);
+int		mini_exit(char **argv);
 void	ft_prev_dir(char *path);
 void	ft_home_dir(void);
 
 //execve
-int	call_cmd(char **av, t_mini *ms);
-int	it_works(char *cmd_path);
+int		call_cmd(char **av, t_mini *ms);
+int		it_works(char *cmd_path);
 
 //pipe
 void	call_pipe_redirect(t_mini *mini, t_token *command, t_token *tok);
-int	redirect_output(t_mini *ms, t_token *token, int type);
-int	redirect_input(t_mini *ms, t_token *token);
+int		redirect_output(t_mini *ms, t_token *token, int type);
+int		redirect_input(t_mini *ms, t_token *token);
 void	here_doc_input(t_token *command, int pid);
-void	handle_pipe_character(const char *s, size_t i, char **result, size_t *j);
+void	handle_pipe_character(const char *s, size_t i,
+			char **result, size_t *j);
 
 //signal
 void	define_signal(void);
 
-
 //var
-MyNode* var_initializeMyNode();
-void free_Var(MyNode* head);
-void var_insertNode(MyNode** head, char* name, char* value);
-char* var_getEnvValue(MyNode *envListHead, char* name);
-void var_printList(MyNode* head);
-
+t_var	*var_initialize_var(void);
+t_var	*var_create_node(char *name, char *value);
+void	free_var(t_var *head);
+void	var_insert_node(t_var **head, char *name, char *value);
+char	*var_get_env_value(t_var *env_list, char *name);
+void	var_print_list(t_var *head);
 
 //env
-Node* getTerminalEnvList(char* env[]);
-char* getEnvValue(Node *envListHead, char* name);
-void free_Head(Node* head);
-char **ft_getenv(Node *envList);
-void printList(Node* head);
-void deleteNode(Node** head, char** name);
-void insertNode(Node** head, char* name, char* value);
+t_node	*get_terminal_env_list(char *env[]);
+char	*get_env_value(t_node *env_list, char *name);
+void	free_head(t_node *head);
+char	**ft_getenv(t_node *envList);
+void	print_list(t_node *head);
+void	delete_node(t_node **head, char **name);
+void	insert_node(t_node **head, char *name, char *value);
 
 //free
 void	agrv_free(char **argv);
