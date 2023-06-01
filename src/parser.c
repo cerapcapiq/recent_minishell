6957b7be4c9b74921ceb747880fa8c59011684f8
cerapcapiq <103482299+cerapcapiq@users.noreply.github.com>
2023-06-01 16:14:54 +0800

    Add files via upload

---
+++ src/parser.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 13:23:40 by abasarud          #+#    #+#             */
+/*   Updated: 2023/06/01 15:12:14 by abasarud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+#include <string.h>
+
+char	*dollar_exit(char *av)
+{
+	char	*exit_status;
+
+	exit_status = NULL;
+	if (!ft_strcmp(av, "$?"))
+		exit_status = ft_itoa(g_exit_num);
+	return (exit_status);
+}
+
+void	create_tokens(t_mini *mini, char **split)
+{
+	t_token	*first;
+	int		i;
+
+	first = NULL;
+	i = 0;
+	first = new_token(mini, *split);
+	mini->tokens = first;
+	while (split[++i])
+		token_addend(split[i], mini);
+}
+
+void	free_parser(char **split, t_mini *mini)
+{
+	token_free(mini->tokens);
+	agrv_free(split);
+}
+
+void	parse(t_mini *mini, char *input)
+{
+	char	**split;
+	int		k;
+	t_token	*first;
+	char	*cpy;
+
+	cpy = NULL;
+	first = NULL;
+	split = NULL;
+	if (input == NULL)
+		exit(0);
+	input = add_spaces_around_pipe(input);
+	cpy = ft_strdup(input);
+	split = ft_new_split(cpy, ' ');
+	k = 0;
+	while (split[k])
+	{
+		if (!ft_strcmp(split[k], "$?"))
+			split[k] = dollar_exit(split[k]);
+		k++;
+	}
+	create_tokens(mini, split);
+	execute(mini);
+	mini->cmd = 1;
+	free_parser(split, mini);
+}
---
+++ src/token.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abasarud <abasarud@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/30 13:23:42 by abasarud          #+#    #+#             */
+/*   Updated: 2023/06/01 15:58:37 by abasarud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+#include <string.h>
+
+int	if_var(char *token)
+{
+	char	c;
+
+	c = '=';
+	while (*token != '\0')
+	{
+		if (*token == c)
+		{
+			printf("the toke n var");
+			return (VAR);
+			break ;
+		}
+		token++;
+	}
+	return (0);
+}
+
+int	token_type(t_mini *mini, char *token)
+{
+	char	*name;
+	char	*value;
+
+	if (delim_token(token))
+	{
+		mini->cmd = 1;
+		return (delim_token(token));
+	}
+	if (!mini->cmd)
+		return (ARG);
+	else
+		mini->cmd = 0;
+	if (builtin_cmd(token))
+		return (BUILTIN);
+	else if (if_var(token))
+	{
+		name = strtok(token, "=");
+		value = strtok(NULL, "=");
+		var_insert_node(&mini->var_list, name, value);
+		return (VAR);
+	}
+	else
+		return (CMD);
+	return (0);
+}
+
+int	token_quote_type(char *data)
+{
+	int	quote;
+
+	if (detect_q(data))
+		quote = 1;
+	else if (ft_double_detect_quote(data))
+		quote = 2;
+	else
+		quote = 0;
+	return (quote);
+}
+
+t_token	*new_token(t_mini *mini, char *data)
+{
+	t_token	*res;
+	char	*new;
+	int		quote_type;
+
+	quote_type = 0;
+	res = (t_token *)malloc(sizeof(t_token));
+	res->quote = token_quote_type(data);
+	new = ft_strdup(data);
+	res->type = token_type(mini, data);
+	res->str = ft_strdup(new);
+	res->prev = 0;
+	res->next = 0;
+	return (res);
+}
+
+void	token_addend(char *data, t_mini *mini)
+{
+	t_token	*tok;
+	t_token	*temp;
+
+	tok = mini->tokens;
+	while (tok->next)
+		tok = tok->next;
+	temp = new_token(mini, data);
+	tok->next = temp;
+	tok->next->prev = tok;
+}
